e1b2652f3e33d77156841d542b48601b7e1f2e7e
Gabriel Okamoto <gabriel.okamoto@intuitivecare.com>
2021-08-25 19:36:01 -0300

    cleanup

---
+++ fix_lookup.py
@@ -0,0 +1,54 @@
+# @Time:   30/06/2021
+# @Author: Gabriel O.
+
+import json
+from tqdm import tqdm
+
+
+def fix_lookup(lookup):
+    """
+    Por algum motivo, a lookup table não anotou corretamente o query id de cada query capturada.
+    Então, para cada query presente na lookup table, iteramos sobre as queries do arquivo original
+    até encontrar o id correto.
+
+    :param lookup:
+    :return:
+    """
+    new_lookup = {}
+    print('Consertando lookup! ~1min')
+    for l_index, l_query in tqdm(lookup['Doc'].items()):
+        real_key = next(m_index for m_index, m_query in marco_query.items() if m_query == l_query)
+        new_lookup[real_key] = l_query
+    return new_lookup
+
+
+if __name__ == '__main__':
+    # Ler os diferentes jsons (queries, answers, well formed answers)
+    with open('data/train_v2.1_query.json', 'r') as marco_query:
+        marco_query = json.load(marco_query)
+
+    with open('data/train_v2.1_answers.json', 'r') as marco_answers:
+        marco_answers = json.load(marco_answers)
+
+    with open('data/train_v2.1_wellFormedAnswers.json', 'r') as marco_wfanswers:
+        marco_wfanswers = json.load(marco_wfanswers)
+
+    with open('lookup_table.json', 'r') as lookupfile:
+        lookup_table = json.load(lookupfile)
+
+    # substituir chaves pelas corretas
+    lookup_table = fix_lookup(lookup_table)
+
+    final = {}
+    print('Formando arquivo final...')
+    for qid, query in tqdm(lookup_table.items()):
+        temp = {
+            'query':            query,
+            'answer':           marco_answers[qid][0],
+            'wellFormedAnswer': marco_wfanswers[qid][0]
+        }
+        final[qid] = temp
+
+    print('Feito! Salvando...')
+    with open('filtered_qna.json', 'w') as finalfile:
+        json.dump(final, finalfile)
--- refilter.py
+++ refilter.py
@@ -5,47 +5,46 @@ Script para filtrar mais uma vez, de forma manual, os resultados filtrados
 por temas.
 """
 
-with open('filtered_qna.json', 'rb') as f:
-    filtered = json.load(f)
+if __name__ == '__main__':
+    with open('filtered_qna.json', 'rb') as f:
+        filtered = json.load(f)
 
-skip = ['hemp', 'canola', 'cbd', 'car', 'tanker', 'salt lake', 'argan', 'town', 'color',
-        'castor', 'synthetic', 'oil change', 'change oil', 'uss', 'almond', 'bergamot',
-        'essential', 'texas', 'tide pod', 'api', 'oregano', 'of the seas']
-always_keep = ['turtle', 'sea wall']
-new = {}
-for i, (qid, qaw) in enumerate(filtered.items()):
-    if i > 1200:
-        break
+    skip = ['hemp', 'canola', 'cbd', 'car', 'tanker', 'salt lake', 'argan', 'town', 'color',
+            'castor', 'synthetic', 'oil change', 'change oil', 'uss', 'almond', 'bergamot',
+            'essential', 'texas', 'tide pod', 'api', 'oregano', 'of the seas']
+    always_keep = ['turtle', 'sea wall']
+    new_json = {}
 
-    should_continue = False
-    for word in skip:
-        if qaw['query'].lower().find(word) >= 0:
-            should_continue = True
-            break
+    for i, (question_id, question_dict) in enumerate(filtered.items()):
+        should_skip = False
+        for word in skip:
+            if question_dict['query'].lower().find(word) >= 0:
+                should_skip = True
+                break
 
-    if should_continue:
-        continue
+        if should_skip:
+            continue
 
-    newqaw = {'query': qaw['query']}
-    wfa = qaw['wellFormedAnswer']
-    newqaw['answer'] = wfa if wfa != '[' else qaw['answer']
+        new_question_dict = {'query': question_dict['query']}
+        wfa = question_dict['wellFormedAnswer']
+        new_question_dict['answer'] = question_dict['answer'] if wfa == '[' else wfa
 
-    keep = False
-    for word in always_keep:
-        if qaw['query'].find(word) >= 0:
-            keep = True
-            break
-    if not keep:
-        print(f"Pergunta #{i}")
-        print('Query: ', newqaw['query'])
-        print('Answer:', newqaw['answer'])
-        keep = input('Manter? ') == 'y'
-    if keep:
-        new[qid] = newqaw
+        keep = False
+        for word in always_keep:
+            if question_dict['query'].find(word) >= 0:
+                keep = True
+                break
+        if not keep:
+            print(f"Pergunta #{i}")
+            print('Query: ', new_question_dict['query'])
+            print('Answer:', new_question_dict['answer'])
+            keep = input('Manter? ') == 'y'
+        if keep:
+            new_json[question_id] = new_question_dict
 
-with open('filtered_qna_.json', 'w') as f:
-    json.dump(new, f)
+    with open('filtered_qna_.json', 'w') as f:
+        json.dump(new_json, f)
 
-
-oops = ['how far can a nation claim in the sea', 'why are rising sea levels a problem',
-        'what resources do we use the ocean for']
+    # how far can a nation claim in the sea
+    # why are rising sea levels a problem
+    # what resources do we use the ocean for
