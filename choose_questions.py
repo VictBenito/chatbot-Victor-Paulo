bf0cd083d0b7610a8d17fc77be236082ee248573
Gabriel Okamoto <gabriel.okamoto@intuitivecare.com>
2021-08-27 01:23:02 -0300

    fixes

--- choose_questions.py
+++ choose_questions.py
@@ -10,17 +10,32 @@ def main():
     df = pd.read_excel('results/Perguntas.xlsx', 'edited', dtype=str)
     df = df.fillna('')
     tag_cols = [t.split('_', 1)[1] for t in df.columns if t.find('tag_') > -1]
-    for i, row in df.iterrows():
+    i = 0
+    # iterar sobre as perguntas
+    while True:
+        row = df.loc[i]
         if row['chatbot?'] and int(row['chatbot?']) >= 0:
             # já preenchi
+            i += 1
             continue
         print('Original:', row['original'])
         print('Pergunta:', row['pergunta'])
+        # tentar obter input do usuário até ele digitar algo válido
         while True:
             inp = input('-- ')
             if inp == '':
                 df.loc[i, 'chatbot?'] = 0
+                i += 1
                 break
+            elif inp == 'b':
+                df.loc[i, 'chatbot?'] = -1
+                i -= 1
+                break
+            elif inp == 's':
+                salvar = input('Salvar? s/n') == 's'
+                if salvar:
+                    df.to_excel('results/Perguntas.xlsx', 'edited')
+                return
             inputs = inp.split(',')
             inputs = list(map(lambda x: x.lower().strip(), inputs))
             try:
@@ -31,10 +46,11 @@ def main():
             tags = inputs[1:]
             for tag in tags:
                 if tag in tag_cols:
-                    df.loc[i, tag_cols[tag]] = 1
+                    df.loc[i, 'tag_'+tag] = 1
                 else:
                     print('Tag não reconhecida:', tag)
             df.loc[i, 'chatbot?'] = chatbot
+            i += 1
             break
 
 
