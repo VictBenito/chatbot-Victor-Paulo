2e7a4e2af9e20a8b913092c203f1568285f6610f
Gabriel Okamoto <gabriel.okamoto@intuitivecare.com>
2021-08-27 18:18:02 -0300

    black

--- choose_questions.py
+++ choose_questions.py
@@ -7,52 +7,55 @@ import pandas as pd
 
 
 def main():
-    df = pd.read_excel('results/Perguntas.xlsx', 'edited', dtype=str)
-    df = df.fillna('')
-    tag_cols = [t.split('_', 1)[1] for t in df.columns if t.find('tag_') > -1]
+    df = pd.read_excel("results/Perguntas.xlsx", "edited", dtype=str)
+    df = df.fillna("")
+    tag_cols = [t.split("_", 1)[1] for t in df.columns if t.find("tag_") > -1]
     i = 0
     # iterar sobre as perguntas
     while True:
         row = df.loc[i]
-        if row['chatbot?'] and int(row['chatbot?']) >= 0:
+        if row["chatbot?"] and int(row["chatbot?"]) >= 0:
             # já preenchi
             i += 1
             continue
-        print('Original:', row['original'])
-        print('Pergunta:', row['pergunta'])
+        print("Original:", row["original"])
+        print("Pergunta:", row["pergunta"])
         # tentar obter input do usuário até ele digitar algo válido
         while True:
-            inp = input('-- ')
-            if inp == '':
-                df.loc[i, 'chatbot?'] = 0
+            inp = input("-- ")
+            if inp == "":
+                df.loc[i, "chatbot?"] = 0
                 i += 1
                 break
-            elif inp == 'b':
-                df.loc[i, 'chatbot?'] = -1
+            elif inp == "b":
+                df.loc[i, "chatbot?"] = -1
                 i -= 1
                 break
-            elif inp == 's':
-                salvar = input('Salvar? s/n') == 's'
+            elif inp == "s":
+                salvar = input("Salvar? s/n") == "s"
                 if salvar:
-                    df.to_excel('results/Perguntas.xlsx', 'edited')
+                    with pd.ExcelWriter(
+                        "results/Perguntas.xlsx", mode="a", if_sheet_exists="replace"
+                    ) as writer:
+                        df.to_excel(writer, sheet_name="edited")
                 return
-            inputs = inp.split(',')
+            inputs = inp.split(",")
             inputs = list(map(lambda x: x.lower().strip(), inputs))
             try:
                 chatbot = int(inputs[0])
             except ValueError:
-                print('Digite no formato: código[, tags]')
+                print("Digite no formato: código[, tags]")
                 continue
             tags = inputs[1:]
             for tag in tags:
                 if tag in tag_cols:
-                    df.loc[i, 'tag_'+tag] = 1
+                    df.loc[i, "tag_" + tag] = 1
                 else:
-                    print('Tag não reconhecida:', tag)
-            df.loc[i, 'chatbot?'] = chatbot
+                    print("Tag não reconhecida:", tag)
+            df.loc[i, "chatbot?"] = chatbot
             i += 1
             break
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
