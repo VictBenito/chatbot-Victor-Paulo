11ab28a14c14b9ba8cb61da1772bab54ffb2bd41
Gabriel Okamoto <gabriel.okamoto@intuitivecare.com>
2021-08-30 21:35:41 -0300

    mostra as tags toda vez

--- choose_questions.py
+++ choose_questions.py
@@ -9,7 +9,7 @@ import pandas as pd
 def main():
     df = pd.read_excel("results/Perguntas.xlsx", "edited", dtype=str)
     df = df.fillna("")
-    tag_cols = [t.split("_", 1)[1] for t in df.columns if t.find("tag_") > -1]
+    available_tags = [t.split("_", 1)[1] for t in df.columns if t.find("tag_") > -1]
     i = 0
     # iterar sobre as perguntas
     while True:
@@ -18,7 +18,11 @@ def main():
             # já preenchi
             i += 1
             continue
-        print(f"Original: {row['original']}\nPergunta: {row['pergunta']}")
+        print(
+            f"Tags disponíveis: {available_tags}\n"
+            f"Original: {row['original']}\n"
+            f"Pergunta: {row['pergunta']}"
+        )
         # tentar obter input do usuário até ele digitar algo válido
         while True:
             input_sequence = input("-- ")
@@ -31,8 +35,7 @@ def main():
                     "Comandos disponíveis:\n"
                     "help (h): mostra esse texto\n"
                     "back (b): volta para a pergunta anterior\n"
-                    "save (s): encerra e salva as mudanças\n"
-                    "tags (t): mostra as tags disponíveis"
+                    "save (s): encerra e salva as mudanças"
                 )
                 df.loc[i - 1, "chatbot?"] = -1
                 i -= 1
@@ -51,11 +54,8 @@ def main():
                         if_sheet_exists="replace",
                     ) as writer:
                         df.to_excel(writer, sheet_name="edited", index=False)
-                        print("Salvo!")
+                        print("\nSalvo!")
                 return
-            elif input_sequence in ("t", "tags"):
-                print("Tags disponíveis:", tag_cols)
-                continue
             inputs = input_sequence.split(",")
             inputs = list(map(lambda x: x.lower().strip(), inputs))
             try:
@@ -65,13 +65,16 @@ def main():
                 continue
             tags = inputs[1:]
             for tag in tags:
-                if tag in tag_cols:
-                    df.loc[i, "tag_" + tag] = 1
-                else:
-                    print("Tag não reconhecida:", tag)
-                    adicionar_tag = input("Adicionar a tag? s/n").lower().strip() == "s"
-                    if adicionar_tag:
-                        df.loc[i, "tag_" + tag] = 1
+                if tag not in available_tags:
+                    adicionar_tag = (
+                        input(f"Tag não reconhecida: {tag}\nAdicionar a tag? s/n")
+                        .lower()
+                        .strip()
+                        == "s"
+                    )
+                    if not adicionar_tag:
+                        continue
+                df.loc[i, "tag_" + tag] = 1
             df.loc[i, "chatbot?"] = chatbot
             i += 1
             break
