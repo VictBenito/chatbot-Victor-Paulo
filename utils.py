e06c4dc42cf63e997f627b748a20720e1118ca15
Gabriel Okamoto <gabriel.okamoto@intuitivecare.com>
2021-08-30 21:52:50 -0300

    refatora getInput

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@
 # @Author: Gabriel O.
 
 
-def getInput(message, dic=None):
+def getInput(message, dic: dict = None):
     """
     Imprime em loop uma mensagem pedindo um input do usuario
     ate que ele digite uma resposta valida.
@@ -15,15 +15,18 @@ def getInput(message, dic=None):
     :return: chave do dicionario correspondente a entrada
     """
     if dic is None:
-        dic = {True:  ['s', 'sim', 'y', 'yes'],
-               False: ['n', 'nao', 'no']}
+        dic = {True: ["s", "sim", "y", "yes"], False: ["n", "nao", "no"]}
+    else:
+        dic = {k.lower().strip() for k in dic.keys()}
     while True:
-        inp = sanitize(input(message))
+        input_str = sanitize(
+            input(f"{message} ({'/'.join([v[0] for k, v in dic.items()])})")
+        )
         try:
-            out = next(key for key, value in dic.items() if inp in value)
+            out = next(key for key, value in dic.items() if input_str in value)
             return out
         except StopIteration:
-            print('Digite uma resposta válida!')
+            print("Digite uma resposta válida!")
 
 
 def sanitize(string) -> str:
@@ -34,15 +37,15 @@ def sanitize(string) -> str:
     :return out: string sem acentos
     """
     out = string.lower().strip()
-    accented = {'a': ['ã', 'á', 'à', 'â'],
-                'e': ['é', 'è', 'ê'],
-                'i': ['í', 'ì', 'î'],
-                'o': ['õ', 'ó', 'ò', 'ô'],
-                'u': ['ú', 'ù', 'û'],
-                'c': ['ç']
-                }
+    accented = {
+        "a": ["ã", "á", "à", "â"],
+        "e": ["é", "è", "ê"],
+        "i": ["í", "ì", "î"],
+        "o": ["õ", "ó", "ò", "ô"],
+        "u": ["ú", "ù", "û"],
+        "c": ["ç"],
+    }
     for letra, acentuadas in accented.items():
         for acc in acentuadas:
-            if acc in out:
-                out = out.replace(acc, letra)
+            out = out.replace(acc, letra)
     return out
