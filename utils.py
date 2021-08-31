195a77e17bed0834f8753e3d61b6b9fbe21c85ae
Gabriel Okamoto <gabriel.okamoto@intuitivecare.com>
2021-08-30 21:36:02 -0300

    adiciona utils

---
+++ utils.py
@@ -0,0 +1,48 @@
+# EP2 Meccomp
+# @File:   utils.py
+# @Time:   29/06/2021
+# @Author: Gabriel O.
+
+
+def getInput(message, dic=None):
+    """
+    Imprime em loop uma mensagem pedindo um input do usuario
+    ate que ele digite uma resposta valida.
+
+    :param message: mensagem que aparece para o usuário
+    :param dic: dicionario onde valores sao respostas do
+                usuario que devolvem a respectiva chave
+    :return: chave do dicionario correspondente a entrada
+    """
+    if dic is None:
+        dic = {True:  ['s', 'sim', 'y', 'yes'],
+               False: ['n', 'nao', 'no']}
+    while True:
+        inp = sanitize(input(message))
+        try:
+            out = next(key for key, value in dic.items() if inp in value)
+            return out
+        except StopIteration:
+            print('Digite uma resposta válida!')
+
+
+def sanitize(string) -> str:
+    """
+    Substitui letras acentuadas pelas nao acentuadas, remove espacos
+    nas pontas e transforma em minusculas.
+    :param string: string a ser sanitizada
+    :return out: string sem acentos
+    """
+    out = string.lower().strip()
+    accented = {'a': ['ã', 'á', 'à', 'â'],
+                'e': ['é', 'è', 'ê'],
+                'i': ['í', 'ì', 'î'],
+                'o': ['õ', 'ó', 'ò', 'ô'],
+                'u': ['ú', 'ù', 'û'],
+                'c': ['ç']
+                }
+    for letra, acentuadas in accented.items():
+        for acc in acentuadas:
+            if acc in out:
+                out = out.replace(acc, letra)
+    return out
