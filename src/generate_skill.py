289161704328643fda9e9515be9e05d8bc14042b
Gabriel Okamoto <gabrielokamoto@hotmail.com>
2021-11-20 16:58:24 -0300

    rename variables

--- src/generate_skill.py
+++ src/generate_skill.py
@@ -13,46 +13,41 @@ from src.entities.entity_operations import get_entities
 from src.io.file_operations import load_questions, load_skill, save_skill
 from src.intents.intent_operations import get_intents
 from src.skills.skill_operations import mix_skills
-from src.utils.list_dict_operations import mix_list
+from src.utils.list_dict_operations import mix_list, remove
 
 
 def main(confidence: float):
     sheet_path = Path(__file__).parent / "../results/Perguntas.xlsx"
     questions = load_questions(sheet_path.resolve().as_posix())
-    questions[~(questions["examples"] == "")].head(3)
 
     skill_path = Path(__file__).parent / "../results/skill-Amazônia-Azul.json"
-    skill = load_skill(skill_path.resolve().as_posix())
+    old_skill = load_skill(skill_path.resolve().as_posix())
 
     new_intents = get_intents(questions)
     print("Intents obtained!")
-    mixed_intents = mix_list(skill["intents"], new_intents)
+    mixed_intents = mix_list(old_skill["intents"], new_intents)
     print("Intents mixed!")
 
     new_entities = get_entities(questions)
     print("Entities obtained!")
     key_priority = {"conditions": 1, "title": 0}
-    mixed_entities = mix_list(skill["entities"], new_entities, key_priority)
+    mixed_entities = mix_list(old_skill["entities"], new_entities, key_priority)
     print("Entities mixed!")
 
     new_nodes = get_dialog_nodes(questions, confidence)
     print("Nodes obtained!")
 
-    old = skill["dialog_nodes"]
-    old = [n for n in old if re.search(r"node_._", n["dialog_node"])]
+    old_nodes = old_skill["dialog_nodes"]
+    old_nodes = [n for n in old_nodes if re.search(r"node_._", n["dialog_node"])]
 
-    mixed_nodes = mix_list(old, new_nodes)
+    mixed_nodes = mix_list(old_nodes, new_nodes)
     print("Nodes mixed!")
 
     node_organizer = NodeOrganizer(mixed_nodes)
     node_organizer.run()
 
     used_intents = node_organizer.get_intents()
-    removed_intents = [
-        intent["intent"]
-        for intent in mixed_intents
-        if intent["intent"] not in used_intents
-    ]
+    removed_intents = remove(mixed_intents, used_intents)
     mixed_intents = [
         intent for intent in mixed_intents if intent["intent"] in used_intents
     ]
@@ -60,7 +55,10 @@ def main(confidence: float):
 
     mixed_nodes = convert_to_list(node_organizer.df)
     mixed_skill = mix_skills(
-        skill, intents=mixed_intents, entities=mixed_entities, dialog_nodes=mixed_nodes,
+        old_skill,
+        intents=mixed_intents,
+        entities=mixed_entities,
+        dialog_nodes=mixed_nodes,
     )
     save_skill(skill_path.resolve().as_posix(), mixed_skill)
     print("Finished at", datetime.now().strftime("%H:%M"))
