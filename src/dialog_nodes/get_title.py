0809d8c88de1ad7ebd074d0f7e40f36bf6e607dc
Gabriel Okamoto <gabrielokamoto@hotmail.com>
2021-11-21 19:34:44 -0300

    move title logic (business rules) to dedicated file

---
+++ src/dialog_nodes/get_title.py
@@ -0,0 +1,224 @@
+# Filter-MSMARCO
+# @File:   get_title.py
+# @Time:   21/11/2021
+# @Author: Gabriel O.
+
+import re
+
+from src.dialog_nodes.dialog_node_operations import get_contexts
+from src.utils.plural import plural
+
+
+def get_title(js: dict) -> str:
+    """
+    Returns a title for a node based on modificador, substantivo, recipiente
+    and rótulos.
+    """
+    modificador = js["modificador"].replace("-", " ")
+    substantivo = js["substantivo"].replace("-", " ")
+    recipiente = js["recipiente"].replace("-", " ")
+
+    contextos = get_contexts(js["rótulos"].replace("-", " ").split("_"))
+    contexto = f"{'/'.join(contextos)}"
+    trechos = []
+
+    if modificador in ["causa"]:
+        if not (contexto or recipiente):
+            raise ValueError(
+                f"Perguntas do tipo 'causa' precisam de recipiente ou contexto!"
+                f" Pergunta: {js['pergunta']}"
+            )
+        trechos.append(modificador)
+        if substantivo:
+            trechos.append(f"de {substantivo}")
+        if recipiente:
+            trechos.append(f"de {recipiente}")
+        if contexto:
+            trechos.append(f"de {contexto}")
+        trechos.append("?")
+    elif modificador in ["definição"]:
+        trechos.append(modificador)
+        if substantivo:
+            trechos.append(f"de {substantivo}")
+        if recipiente:
+            trechos.append(f"de {recipiente}?")
+        else:
+            trechos.append(f"de {contexto}?")
+    elif modificador in ["detalhar"]:
+        if not (contexto or recipiente):
+            raise ValueError(
+                f"Perguntas do tipo 'detalhar' precisam de recipiente ou de contexto! "
+                f"Pergunta: {js['pergunta']}"
+            )
+        trechos.append(modificador)
+        if substantivo:
+            trechos.append(f"{substantivo}")
+        if recipiente:
+            trechos.append(f"de {recipiente}")
+        if contexto:
+            trechos.append(f"de {contexto}")
+    elif modificador in ["diferença"]:
+        trechos.append(modificador)
+        trechos.append(f"entre {substantivo}")
+        trechos.append(f"e {recipiente or contextos[0]}?")
+    elif modificador in ["é"]:
+        if not (
+            (substantivo and contexto)
+            or (recipiente and contexto)
+            or (substantivo and recipiente)
+        ):
+            raise ValueError(
+                f"Perguntas do tipo 'é' precisam de dois entre substantivo, recipiente "
+                f"e contexto! Pergunta: {js['pergunta']}"
+            )
+        else:
+            trechos.append(substantivo or contexto)
+            trechos.append(f"é {recipiente or contexto}?")
+    elif modificador in ["efeito"]:
+        trechos.append(modificador)
+        if substantivo:
+            trechos.append(f"de {substantivo}")
+        if recipiente:
+            if contexto:
+                trechos.append(f"de {contexto}")
+            trechos.append(f"em {recipiente}")
+        else:
+            trechos.append(f"em {contexto}")
+        trechos.append("?")
+    elif modificador in ["existe"]:
+        if not recipiente:
+            raise ValueError(
+                f"Perguntas do tipo 'existe' precisam de recipiente! "
+                f"Pergunta: {js['pergunta']}"
+            )
+        if contexto:
+            trechos.append(contexto + ":")
+        trechos.append(modificador)
+        if substantivo:
+            trechos.append(substantivo)
+        trechos.append(f"em {recipiente}?")
+    elif modificador in ["explicar"]:
+        trechos.append(modificador)
+        if substantivo:
+            trechos.append(substantivo)
+            if recipiente:
+                trechos.append(f"de {recipiente}")
+            if contexto:
+                trechos.append(f"de {contexto}")
+        else:
+            trechos.append(contexto)
+    elif modificador in ["listar"]:
+        if substantivo:
+            if recipiente:
+                if contexto:
+                    trechos.append(f"{contexto}:")
+                trechos.append(modificador)
+                trechos.append(plural(substantivo))
+                trechos.append(f"de {recipiente}")
+            else:
+                trechos.append(modificador)
+                trechos.append(plural(substantivo))
+                if contexto:
+                    trechos.append(f"de {contexto}")
+        else:
+            trechos.append(modificador)
+            trechos.append(contexto)
+    elif modificador in ["maior", "menor"]:
+        trechos.append(modificador)
+        if substantivo:
+            trechos.append(substantivo)
+        if contexto:
+            trechos.append(f"de {contexto}")
+        if recipiente:
+            trechos.append(f"de {recipiente}")
+        trechos.append("?")
+    elif modificador in ["maiores", "menores"]:
+        trechos.append(modificador)
+        trechos.append(plural(substantivo))
+        if contexto:
+            trechos.append(f"de {contexto}")
+        if recipiente:
+            trechos.append(f"de {recipiente}")
+        trechos.append("?")
+    elif modificador in ["onde"]:
+        if not (contexto or recipiente):
+            raise ValueError(
+                f"Perguntas do tipo 'detalhar' precisam de recipiente ou contexto!"
+                f" Pergunta: {js['pergunta']}"
+            )
+        trechos.append(f"{modificador} tem")
+        if not (substantivo and recipiente):
+            trechos.append(contexto)
+        elif substantivo:
+            trechos.append(plural(substantivo))
+            trechos.append(f"de {recipiente or contexto}")
+        else:
+            trechos.append(contexto)
+            trechos.append(f"em {recipiente}")
+        trechos.append("?")
+    elif modificador in ["porque"]:
+        if not contexto:
+            raise ValueError(
+                f"Perguntas do tipo 'porque' precisam de contexto!"
+                f" Pergunta: {js['pergunta']}"
+            )
+        trechos.append(f"{contexto}:")
+        trechos.append("motivo para")
+        if recipiente:
+            trechos.append(recipiente)
+        trechos.append(f"?")
+    elif modificador in ["quantidade"]:
+        if not (substantivo or contexto):
+            raise ValueError(
+                f"Perguntas do tipo 'quantidade' precisam de substantivo ou contexto!"
+                f" Pergunta: {js['pergunta']}"
+            )
+        if substantivo:
+            trechos.append(f"{contexto}:")
+            trechos.append(modificador)
+            trechos.append(f"de {plural(substantivo)}")
+        else:
+            trechos.append(modificador)
+            trechos.append(f"de {contexto}")
+        if recipiente:
+            trechos.append(f"em {recipiente}")
+        trechos.append("?")
+    elif modificador in ["responsável"]:
+        if not substantivo:
+            raise ValueError(
+                f"Perguntas do tipo 'responsável' precisam de substantivo! "
+                f"Pergunta: {js['pergunta']}"
+            )
+        if not (contexto or recipiente):
+            raise ValueError(
+                f"Perguntas do tipo 'responsável' precisam de recipiente ou contexto! "
+                f"Pergunta: {js['pergunta']}"
+            )
+        trechos.append(modificador)
+        trechos.append(f"por {substantivo}")
+        trechos.append(f"de {recipiente or contexto}?")
+    else:
+        raise NotImplementedError(f"Modificador '{modificador}' não programado!")
+    trechos[0] = trechos[0].capitalize()
+    titulo = " ".join(trechos)
+    titulo = titulo.strip()
+    substituir = {
+        r"\bem amazônia azul": "na Amazônia Azul",
+        r"\bde amazônia azul": "da Amazônia Azul",
+        r"\bem brasil": "no Brasil",
+        r"\bde brasil": "do Brasil",
+        r"\bem oceano": "no oceano",
+        r"\bde oceano": "do oceano",
+        r"\bde governo": "do governo",
+        r"\bde mundo": "do mundo",
+        r"\bem ambiente": "no ambiente",
+        r"\bde branqueamento": "do branqueamento",
+        r"\bde poluição": "da poluição",
+        r"\bum tartaruga": "uma tartaruga",
+        r"\bde projeto de": "do projeto",
+        r"\s+\?": "?",
+        r"\s+": " ",
+    }
+    for pattern, subs in substituir.items():
+        titulo = re.sub(pattern, subs, titulo)
+    return titulo
