d9987786e8efe64f25b1cbe62453a3a28ae10d06
Gabriel Okamoto <gabrielokamoto@hotmail.com>
2021-11-22 15:23:46 -0300

    improve sorting of nodes and apply previous siblings after sorting

--- src/dialog_nodes/Node.py
+++ src/dialog_nodes/Node.py
@@ -10,6 +10,7 @@ from dataclasses import dataclass, field
 from typing import Dict, List
 
 from src.utils.list_dict_operations import drop_empty
+from src.utils.sanitize import sanitize
 
 
 @dataclass
@@ -35,16 +36,31 @@ class Node:
 
     def add_child(self, node: Node):
         node.parent = self.dialog_node
-        if self.children:
-            node.previous_sibling = self.children[-1].dialog_node
         self.children.append(node)
 
-    def to_list(self) -> List[dict]:
+    def to_list(self) -> List[NodeDict]:
         out = [self.to_dict()]
         for node in self.children:
             out += node.to_list()
-        out.sort(key=lambda x: x.get("intent", ""))
+        # the priority of the sort key is higher on later sorts
+        # 'zzzzz' is used to ensure that empty values go last without needing to reverse
+        out.sort(key=lambda x: sanitize(x.get("recipiente", "zzzzz")))
+        out.sort(key=lambda x: sanitize(x.get("substantivo", "zzzzz")))
+        out.sort(key=lambda x: sanitize(x.get("modificador", "zzzzz")))
+        out.sort(key=lambda x: sanitize(x.get("rotulos", "")))
+        out = self.apply_previous_siblings(out)
         return out
 
-    def to_dict(self) -> dict:
+    def to_dict(self) -> NodeDict:
         return drop_empty(self.__dict__)
+
+    @staticmethod
+    def apply_previous_siblings(nodes: List[NodeDict]) -> List[NodeDict]:
+        for i, node in enumerate(nodes):
+            if i == 0:
+                continue
+            node["previous_sibling"] = nodes[i - 1]["dialog_node"]
+        return nodes
+
+
+NodeDict = Node.__annotations__
