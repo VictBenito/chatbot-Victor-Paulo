05582f9e31245a79d285108754b1f2459ddb9e68
Gabriel Okamoto <gabrielokamoto@hotmail.com>
2021-12-02 21:48:19 -0300

    add type hints

--- src/entities/entity_operations.py
+++ src/entities/entity_operations.py
@@ -3,10 +3,14 @@
 # @Time:   20/11/2021
 # @Author: Gabriel O.
 
+from typing import List
+
+import pandas as pd
+
 from src.utils.list_dict_operations import drop_duplicates
 
 
-def get_entities(df):
+def get_entities(df: pd.DataFrame) -> List[dict]:
     subset = ["rótulos", "modificador", "substantivo", "recipiente"]
     entities = [
         {"entity": col, "values": get_entity_values(df[col]), "fuzzy_match": True}
@@ -16,7 +20,7 @@ def get_entities(df):
     return entities
 
 
-def get_entity_values(series):
+def get_entity_values(series: pd.Series) -> List[dict]:
     records = series.to_list()
     records = drop_duplicates(records)
     records = [r.replace("-", " ") for r in records]
